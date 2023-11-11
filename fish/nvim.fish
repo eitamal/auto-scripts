73eb8ab92a0d81991b797ce76559ed2e5b94ac19
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-11 21:25:49 +1000

    fix(fish): fix the path to the chatgpt-nvim script in the nvim shadow fish function

--- fish/nvim.fish
+++ fish/nvim.fish
@@ -1,11 +1,4 @@
-# function nvim --description "Run Neovim with with the ChatGPT.nvim OpenAI API key" --wraps nvim
-#     set --local --export OPENAI_API_KEY $(get_api_key "openai/chatgpt.nvim" 2> /dev/null | string trim)
-#     if test -z "$OPENAI_API_KEY"
-#         echo "No API key retrieved for the key: $_flag_key" >&2 && return 1
-#     end
-#     command nvim $argv
-# end
-
-function nvim
-    OPENAI_API_KEY=(/home/pluto/dev/scripts/chatgpt-nvim.sh) command nvim $argv
+function nvim --description "Run Neovim with with the ChatGPT.nvim OpenAI API key" --wraps nvim
+    set --local script_path realpath "$(status dirname)/../chatgpt-nvim.sh"
+    OPENAI_API_KEY=($script_path) command nvim $argv
 end
