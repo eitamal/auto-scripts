b3124aba89ed08268a41b4ed1e0ceaaf8b75dc07
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-11 17:20:26 +1000

    feat(installers): add an installer of all the Go tools I use

---
+++ installers/go-tools.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+go install golang.org/x/tools/cmd/goimports@latest 
+go install mvdan.cc/gofumpt@latest 
+go install golang.org/x/tools/gopls@latest 
+go install github.com/ramya-rao-a/go-outline@latest 
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/josharian/impl@latest
+go install github.com/cweill/gotests/gotests@latest
+go install github.com/fatih/gomodifytags@latest
+
+# golangci-lint
+# binary will be $(go env GOPATH)/bin/golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.2
+
