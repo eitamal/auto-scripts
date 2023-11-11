55d6b28dadc4a64c26b6c1a16e9b6ce67536e2e6
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-11 21:51:07 +1000

    feat(fish): remove setting the environment variable since it's now done by the chatgpt.nvim plugin

--- fish/nvim.fish
+++ fish/nvim.fish
@@ -1,5 +1,5 @@
 function nvim --description "Run Neovim with with the ChatGPT.nvim OpenAI API key" --wraps nvim
     set --local script_path realpath "$(status dirname)/../chatgpt-nvim.sh"
     # HACK: call the script once to overcome the pinentry prompt issue
-    $script_path >/dev/null; and OPENAI_API_KEY=($script_path) command nvim $argv
+    $script_path >/dev/null; and command nvim $argv
 end
