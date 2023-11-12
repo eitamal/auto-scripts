67af107f0bc1ee97e28b05df0f0949a305f4c0e9
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-12 20:19:04 +1000

    feat(fish): replace the use of the chatgpt-nvim script for the pinentry trigger with the new pinentry trigger script

--- fish/nvim.fish
+++ fish/nvim.fish
@@ -1,5 +1,4 @@
 function nvim --description "Run Neovim with with the ChatGPT.nvim OpenAI API key" --wraps nvim
-    set --local script_path realpath "$(status dirname)/../chatgpt-nvim.sh"
-    # HACK: call the script once to overcome the pinentry prompt issue
-    $script_path >/dev/null; and command nvim $argv
+    set --local script_path realpath
+    sh (realpath "$(status dirname)/../util/pinentry-trigger.sh"); and command nvim $argv
 end
