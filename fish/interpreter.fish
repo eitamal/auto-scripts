d6e2c0cef3fda92a61ece84da4716e9814934b1d
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-12 20:24:19 +1000

    feat(fish): change the OpenAI shadows to set the env var directly by calling pass

--- fish/interpreter.fish
+++ fish/interpreter.fish
@@ -1,4 +1,4 @@
 # Override the open-interpreter command with the one that uses the OpenAI API key
-function interpreter --description "Run Open Interpreter with it's corresponding OpenAI API key" --wraps interpreter
-    with_openai open-interpreter $(command -s interpreter) $argv
+function interpreter --description "Run open-interpreter with it's corresponding OpenAI API key" --wraps interpreter
+    OPENAI_API_KEY=(pass openai/interpreter) $argv
 end
--- fish/sgpt.fish
+++ fish/sgpt.fish
@@ -1,4 +1,4 @@
 # Override the shell_gpt command with the one that uses the OpenAI API key
 function sgpt --description "Run shell_gpt with it's corresponding OpenAI API key" --wraps sgpt
-    with_openai sgpt $(command -s sgpt) $argv
+    OPENAI_API_KEY=(pass openai/sgpt) $argv
 end
