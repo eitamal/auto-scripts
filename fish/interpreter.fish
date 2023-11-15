ad1ee401ec9f8b41141c0ee0a151c2a20cfddb83
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-15 10:45:33 +1000

    fix(fish): fix sgpt and interpreter functions to actually call their corresponding commands

--- fish/interpreter.fish
+++ fish/interpreter.fish
@@ -1,4 +1,4 @@
 # Override the open-interpreter command with the one that uses the OpenAI API key
 function interpreter --description "Run open-interpreter with it's corresponding OpenAI API key" --wraps interpreter
-    OPENAI_API_KEY=(pass openai/interpreter) $argv
+    OPENAI_API_KEY=(pass openai/interpreter) command interpreter $argv
 end
--- fish/sgpt.fish
+++ fish/sgpt.fish
@@ -1,4 +1,4 @@
 # Override the shell_gpt command with the one that uses the OpenAI API key
 function sgpt --description "Run shell_gpt with it's corresponding OpenAI API key" --wraps sgpt
-    OPENAI_API_KEY=(pass openai/sgpt) $argv
+    OPENAI_API_KEY=(pass openai/sgpt) command sgpt $argv
 end
