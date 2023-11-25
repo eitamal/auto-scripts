5ca5b34bd7c5403b0647b4cfbcdd9e7a1fcd906b
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-25 22:43:36 +1000

    feat(installers): add tools to the go tools installer and refactor to use an array

--- installers/go-tools.sh
+++ installers/go-tools.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
-go install golang.org/x/tools/cmd/goimports@latest 
-go install mvdan.cc/gofumpt@latest 
-go install golang.org/x/tools/gopls@latest 
-go install github.com/ramya-rao-a/go-outline@latest 
-go install github.com/go-delve/delve/cmd/dlv@latest
-go install github.com/josharian/impl@latest
-go install github.com/cweill/gotests/gotests@latest
-go install github.com/fatih/gomodifytags@latest
+pkgs=(
+    golang.org/x/tools/cmd/goimports@latest
+    mvdan.cc/gofumpt@latest
+    golang.org/x/tools/gopls@latest
+    github.com/ramya-rao-a/go-outline@latest
+    github.com/go-delve/delve/cmd/dlv@latest
+    github.com/josharian/impl@latest
+    github.com/cweill/gotests/gotests@latest
+    github.com/fatih/gomodifytags@latest
+    go.uber.org/mock/mockgen@latest
+    github.com/kyoh86/richgo@latest
+    golang.org/x/vuln/cmd/govulncheck@latest
+    github.com/onsi/ginkgo/v2/ginkgo@latest
+    github.com/segmentio/golines@latest
+    github.com/davidrjenni/reftools/cmd/fillstruct@latest
+    golang.org/x/tools/cmd/guru@latest
+    github.com/motemen/go-iferr/cmd/goiferr@latest
+    honnef.co/go/tools/cmd/staticcheck@latest
+)
+
+for pkg in "${pkgs[@]}"; do
+    go install "$pkg"
+done
 
 # golangci-lint
 # binary will be $(go env GOPATH)/bin/golangci-lint
