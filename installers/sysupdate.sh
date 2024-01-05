10d9c076b3b7d43adb53b32c35ba9b63435e7a9b
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2024-01-05 11:58:43 +1000

    feat(installers): update rtx updater to use the renamed version - mise

--- installers/sysupdate.sh
+++ installers/sysupdate.sh
@@ -6,7 +6,7 @@ sudo apt update &&
 	sudo apt autoclean &&
 	sudo apt autoremove
 
-rtx self-update --yes
+mise self-update --yes
 
 "$(realpath "$(dirname "$0")/../util/gh-update.sh")" bat sharkdp/bat "bat --version" '"^bat_.+amd64\\.deb"'
 
